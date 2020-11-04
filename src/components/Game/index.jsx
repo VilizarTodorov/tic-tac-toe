bbf5a6551df64768d27b38c9cfb679530cd05b6c
VilizarTodorov <todorovvilizar@gmail.com>
2020-11-04 02:13:04 +0200

    fixed board transition

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -6,6 +6,7 @@ import "./styles.css";
 
 const INITIAL_STATE = {
   isPlaying: false,
+  component: []
 };
 
 class Game extends React.Component {
@@ -14,30 +15,32 @@ class Game extends React.Component {
     this.state = { ...INITIAL_STATE };
   }
 
+  componentDidMount(){
+    this.setState({component:<InitialGameBoardScreen startGame={this.startGame}></InitialGameBoardScreen>})
+  }
+
   startGame = () => {
-    this.setState((state) => ({
-      isPlaying: !state.isPlaying,
-    }));
+    this.setState({component:[]},() => setTimeout(() => this.setState({component:<GameBoard></GameBoard>}),700));
   };
 
   render() {
+    const { component } = this.state;
+
     return (
       <div className="game">
         <Transition
-          items={this.state.isPlaying}
-          from={{ transform: "rotateX(45deg) rotateY(90deg)", height: "400px" }}
+          items={component}
+          from={{
+            transform: "rotateX(45deg) rotateY(90deg)",
+            height: "400px",
+            position: "absolute",
+            width: "400px",
+          }}
           enter={{ transform: "rotateX(0deg) rotateY(0)" }}
-          leave={{ background:'white' }}
+          leave={{ transform: "rotateX(45deg) rotateY(90deg)" }}
+          config={{ ...config.wobbly,duration:700 }}
         >
-          {(isPlaying) =>
-            isPlaying
-              ? (props) => <div style={props}>{<GameBoard></GameBoard>}</div>
-              : (props) => (
-                  <div style={props}>
-                    <InitialGameBoardScreen startGame={this.startGame}></InitialGameBoardScreen>
-                  </div>
-                )
-          }
+          {(component) => (props) => <div style = {props}>{component}</div>}
         </Transition>
       </div>
     );
@@ -45,3 +48,22 @@ class Game extends React.Component {
 }
 
 export default Game;
+
+{
+  /* <Transition
+  items={this.state.isPlaying}
+  from={{ transform: "rotateX(45deg) rotateY(90deg)", height: "400px" }}
+  enter={{ transform: "rotateX(0deg) rotateY(0)" }}
+  leave={{ background:'white' }}
+>
+  {(isPlaying) =>
+    isPlaying
+      ? (props) => <div style={props}>{<GameBoard></GameBoard>}</div>
+      : (props) => (
+          <div style={props}>
+            <InitialGameBoardScreen startGame={this.startGame}></InitialGameBoardScreen>
+          </div>
+        )
+  }
+</Transition> */
+}
