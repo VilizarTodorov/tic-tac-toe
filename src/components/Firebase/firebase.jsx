d564cead02f4ee98b2e4425b2cc40f3c9deafa3d
VilizarTodorov <todorovvilizar@gmail.com>
2021-04-08 15:39:26 +0300

    minor changes for the exam

--- src/components/Firebase/firebase.jsx
+++ src/components/Firebase/firebase.jsx
@@ -2,27 +2,37 @@ import app from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-const devConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-};
+// const devConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_DATABASE_URL,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+// };
 
-const prodConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-};
+// const prodConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_DATABASE_URL,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+// };
 
-const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
+// const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
+
+const config = {
+  apiKey: "AIzaSyBC1KRSVn97cX6GvXlFaS1pEvtj7dB6BUk",
+  authDomain: "project-2a849.firebaseapp.com",
+  databaseURL: "https://project-2a849-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "project-2a849",
+  storageBucket: "project-2a849.appspot.com",
+  messagingSenderId: "344950076929",
+  appId: "1:344950076929:web:56feb73f94eba5bf667200"
+};
 
 class Firebase {
   constructor() {
